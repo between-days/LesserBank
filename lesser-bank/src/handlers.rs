53f2fdb53c35167f8b9ff1317a7b68168ea3ff5c
Jack Carney <jackscottcarney@gmail.com>
2023-07-03 15:40:23 +1000

    rust actix setup

---
+++ lesser-bank/src/handlers.rs
@@ -0,0 +1,32 @@
+use actix_web::{web, Result};
+
+pub async fn create_account(path: web::Path<(u32, u32)>) -> Result<String> {
+    let (customer_id, account_id) = path.into_inner();
+    Ok(format!(
+        "Trying to create account {}, for customer {}",
+        account_id, customer_id,
+    ))
+}
+
+pub async fn get_accounts(path: web::Path<u32>) -> Result<String> {
+    let customer_id = path.into_inner();
+    Ok(format!(
+        "Trying to get accounts for customer {}",
+        customer_id,
+    ))
+}
+pub async fn get_account(path: web::Path<(u32, u32)>) -> Result<String> {
+    let (customer_id, account_id) = path.into_inner();
+    Ok(format!(
+        "Trying to get account {}, for customer {}",
+        account_id, customer_id
+    ))
+}
+
+pub async fn delete_account(path: web::Path<(u32, u32)>) -> Result<String> {
+    let (customer_id, account_id) = path.into_inner();
+    Ok(format!(
+        "Trying to delete account {}, for customer {}",
+        account_id, customer_id
+    ))
+}
---
+++ lesser-bank/src/main.rs
@@ -0,0 +1,46 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+mod handlers;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route(
+                "/customers/{cid}/accounts/{aid}",
+                web::post().to(handlers::create_account),
+            )
+            .route(
+                "/customers/{cid}/accounts",
+                web::get().to(handlers::get_accounts),
+            )
+            .route(
+                "/customers/{cid}accounts/{aid}",
+                web::get().to(handlers::get_account),
+            )
+            .route(
+                "/customers/{cid}/accounts/{aid}",
+                web::delete().to(handlers::delete_account),
+            )
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
