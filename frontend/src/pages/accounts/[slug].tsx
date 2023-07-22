00642aad374ffca4ca7d8b6cffc3afd642a6792a
between-days <jackscottcarney@gmail.com>
2023-07-23 03:32:19 +1000

    remove temp content/ components

--- frontend/src/pages/accounts/[slug].tsx
+++ frontend/src/pages/accounts/[slug].tsx
@@ -1,31 +1,105 @@
 import TransactionItem from "@/components/transaction/TransactionItem";
 import { mockTransactions } from "@/mockBackend";
-import { Space, } from "@mantine/core";
+import { Grid, Group, Skeleton, Space, Stack, Title, useMantineTheme, } from "@mantine/core";
 import React from 'react';
-import { AccountDetailContent } from "../../components/account/content/AccountDetailsContent";
+import CustomPage from "@/components/shared/CustomPage";
+import { AccountNumberAndTypeInfoPaper } from "@/components/account/AccountNumberAndTypeInfoPaper";
+import { AccountOpenedInfoPaper } from "@/components/account/AccountOpenedInfoPaper";
+import { AccountBalancesInfoPaper } from "@/components/account/AccountBalancesInfoPaper";
+import { useOneAccount } from "@/services/AccountsService";
+import { InternalErrorContent } from "@/components/shared/error/InternalErrorContent";
 
 export const getServerSideProps = async (context: { query: { slug: any; }; }) => {
     let { slug } = context.query;
     return { props: { slug: slug } };
 };
 
+const AccountDetailsLoadingContent = ({ children }: { children: any }) => {
+    return (
+        <CustomPage title={
+            <Title color="dimmed" >
+                Loading Account Details...
+            </Title>
+        }>
+            <Grid gutter="md">
+                <Grid.Col xs={4}>
+                    <Skeleton h="11rem" pl="5rem" radius="lg" />
+                </Grid.Col>
+                <Grid.Col xs={4}>
+                    <Skeleton h="11rem" pl="5rem" radius="lg" />
+                </Grid.Col>
+                <Grid.Col xs={4}>
+                    <Stack align="stretch" justify="center">
+                        <Skeleton w="20rem" h="5rem" radius="lg" />
+                        <Skeleton w="20rem" h="5rem" radius="lg" />
+                    </Stack>
+                </Grid.Col>
+
+                <Grid.Col xs={12} >
+                    <div>
+                        <Title order={2} color="dimmed">Transactions</Title>
+                        <Space mb="md" />
+                        {children}
+                    </div>
+                </Grid.Col>
+            </Grid>
+        </CustomPage>
+    )
+}
+
 export default function AccountDetail(props: { slug: any; }) {
     const { slug } = props
 
-    if (!slug) return <></>
+    const theme = useMantineTheme()
 
     const accountNumber = Number(slug)
 
+    const { data, error } = useOneAccount({ accountNumber })
     const transactions = mockTransactions()
+
+    if (error) return <InternalErrorContent />
+
     const transactionsContent = transactions.map((transaction, i) =>
         <div key={i}>
             <TransactionItem {...transaction} />
             <Space h="md" />
         </div>)
 
+    if (!accountNumber || !data) return <AccountDetailsLoadingContent>
+        {transactionsContent}
+    </AccountDetailsLoadingContent>
+
+    const account = data
+
+    const { name, dateOpened, balanceCents, availableBalanceCents, accountType, bsb } = account
+
     return (
-        <AccountDetailContent {...{ accountNumber }}>
-            {transactionsContent}
-        </AccountDetailContent>
+        <CustomPage title={
+            <Group>
+                <Title order={2}>Account Details for</Title>
+                <Title order={2} color={theme.primaryColor} italic>
+                    {name ? name : accountNumber}
+                </Title>
+            </Group>
+        }>
+            <Grid>
+                <Grid.Col xs={3}>
+                    <AccountBalancesInfoPaper availableBalanceCents={availableBalanceCents} balanceCents={balanceCents} />
+                </Grid.Col>
+                <Grid.Col xs={4}>
+                    <Stack align="stretch" justify="center">
+                        <AccountNumberAndTypeInfoPaper accountNumber={accountNumber} accountType={accountType} bsb={bsb} />
+                        <AccountOpenedInfoPaper dateOpened={dateOpened} />
+                    </Stack>
+                </Grid.Col>
+                <Grid.Col xs={12} >
+                    <div>
+                        <Title order={2}>Transactions</Title>
+                        <Space mb="md" />
+                        {transactionsContent}
+                    </div>
+                </Grid.Col>
+            </Grid>
+        </CustomPage>
     )
 }
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,16 +1,56 @@
-import { Title } from '@mantine/core';
+import { Affix, Button, Grid, SegmentedControl, Title, rem } from '@mantine/core';
 import React from 'react';
 import CustomPage from '@/components/shared/CustomPage';
-import { DashboardContent } from '../components/account/content/DashboardContent';
-import { DashboardAffixes } from '../components/account/content/DashboardAffixes';
+import { IconSquareRoundedPlus } from '@tabler/icons-react';
+import AccountCard from '@/components/account/AccountCard';
+import { InternalErrorContent } from '@/components/shared/error/InternalErrorContent';
+import { useAccounts } from '@/services/AccountsService';
+
+const DashboardAffixes = () => {
+    return <>
+        <Affix position={{ bottom: rem(20), left: rem(20) }}>
+            <Button
+                radius="md"
+                size="md"
+                variant="gradient"
+                gradient={{ from: 'orange', to: 'red' }}
+                leftIcon={<IconSquareRoundedPlus size="1.5rem" />}
+                onClick={() => { }}
+            >
+                New Account
+            </Button>
+        </Affix>
+        <Affix position={{ top: rem(50), left: rem(20) }}>
+            <SegmentedControl
+                data={[
+                    { label: 'Grid', value: 'grid' },
+                    { label: 'Type', value: 'type', disabled: true },
+                ]}
+            />
+        </Affix>
+    </>
+}
 
 export default function Dashboard() {
+    const { data, error } = useAccounts();
+
+    if (error) return <InternalErrorContent />
+    if (!data) return <>Loading...</>
+
+    const accounts = data
+
     return (
         <CustomPage title={
             <Title order={2}>Accounts</Title>
         }>
             <DashboardAffixes />
-            <DashboardContent />
+            <Grid gutter="xl">
+                {accounts.map((account, i) =>
+                    <Grid.Col md={4} key={i}>
+                        <AccountCard {...{ ...account, onHover: true }} />
+                    </Grid.Col>
+                )}
+            </Grid>
         </CustomPage>
     )
 }
